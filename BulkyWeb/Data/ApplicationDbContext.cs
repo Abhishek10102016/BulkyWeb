7ad2dca925023ebfc80456a6a6f76a05fffaa011
Abhishek10102016 <abhisheklovesthakur@gmail.com>
2023-06-26 17:35:10 +0530

    Added ConnectionString and applicationDbContext

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
